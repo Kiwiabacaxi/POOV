916cfd0373041f46fde712fe6bcc8b8885d42080
Carlos <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-08-24 10:55:32 -0300

    Atributos privados

--- ProjetoLampada/src/App.java
+++ ProjetoLampada/src/App.java
@@ -2,14 +2,25 @@ public class App {
     public static void main(String[] args) {
         // chamar a classe Lampada
         Lampada lampada = new Lampada();
-        lampada.tensao = 220;
-        lampada.potencia = 60;
+
+/*      
+        // como os atributos são privados, não é possível acessá-los diretamente
+        lampada.tensao = 9999999;
+        lampada.potencia = 69;
         lampada.ligada = true;
         lampada.quebrada = false;
-        lampada.queimada = false;
+        lampada.queimada = false; */
 
         lampada.mostrar();
 
+        // chamar a classe Lampada com construtor Inicialização (t, p)
+        Lampada lampada2 = new Lampada(220, 60);
+        lampada2.mostrar();
+        
+        // chamar a classe Lampada com construtor Inicialização (t, p, a)
+        Lampada lampada3 = new Lampada(220, 60, true);
+        lampada3.mostrar();
+
     }
-    
+
 }
--- ProjetoLampada/src/Lampada.java
+++ ProjetoLampada/src/Lampada.java
@@ -1,15 +1,46 @@
 public class Lampada {
     // Atributos
-    public int tensao; // 110 220
-    public int potencia; // 40 60 100 200
-    public boolean ligada; // true false
-    public boolean quebrada; // true false
-    public boolean queimada; // true falseO
+    private int tensao; // 110 220
+    private int potencia; // 40 60 100 200
+    private boolean ligada; // true false
+    private boolean quebrada; // true false
+    private boolean queimada; // true falseO
+
+    // construtor padrao
+    public Lampada() {
+        System.out.println("Construtor padrão");
+        this.tensao = 110;
+        this.potencia = 40;
+        this.ligada = false;
+        this.quebrada = false;
+        this.queimada = false;
+    }
+
+    // construtor Inicialização (t, p)
+    public Lampada(int t, int p) {
+        System.out.println("Construtor Inicialização (t, p)");
+        this.tensao = t;
+        this.potencia = p;
+        this.ligada = false;
+        this.quebrada = false;
+        this.queimada = false;
+    }
+
+    // construtor Inicialização (t, p, a)
+    public Lampada(int t, int p, boolean a) {
+        System.out.println("Construtor Inicialização (t, p, a)");
+        this.tensao = t;
+        this.potencia = p;
+        this.ligada = a;
+        this.quebrada = false;
+        this.queimada = false;
+    }
 
     public void mostrar() {
-        String texto = "Tensão: " + tensao + "V\n" + "Potencia: " + potencia + "W\n" + "Esta ligada: " +((ligada) ? "Ligada\n" : "Apagada\n")
-                + ((quebrada) ? "Quebrada\n" : "Inteira\n")
-                + ((queimada) ? "Queimada\n" : "Inteira\n");
+        String texto = "Tensão: " + tensao + "V\n" + "Potencia: " + potencia + "W\n" + "Esta ligada: "
+                + ((ligada) ? "Ligada\n" : "Apagada\n")
+                + "A lampada esta: " + ((quebrada) ? "Quebrada\n" : "Inteira\n")
+                + "A lampada esta: " + ((queimada) ? "Queimada\n" : "Inteira\n");
 
         System.out.println(texto);
     }
