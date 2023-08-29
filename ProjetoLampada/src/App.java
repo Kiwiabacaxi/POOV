ab846db3aae724558c1fe339028f752b391d2a38
Carlos <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-08-29 08:58:06 -0300

    ultimo filho bastardo da lampada ?

--- ProjetoLampada/src/App.java
+++ ProjetoLampada/src/App.java
@@ -3,23 +3,35 @@ public class App {
         // chamar a classe Lampada
         Lampada lampada = new Lampada();
 
-/*      
-        // como os atributos são privados, não é possível acessá-los diretamente
-        lampada.tensao = 9999999;
-        lampada.potencia = 69;
-        lampada.ligada = true;
-        lampada.quebrada = false;
-        lampada.queimada = false; */
+        /*
+         * // como os atributos são privados, não é possível acessá-los diretamente
+         * lampada.tensao = 9999999;
+         * lampada.potencia = 69;
+         * lampada.ligada = true;
+         * lampada.quebrada = false;
+         * lampada.queimada = false;
+         */
 
         lampada.mostrar();
 
-        // chamar a classe Lampada com construtor Inicialização (t, p)
-        Lampada lampada2 = new Lampada(220, 60);
-        lampada2.mostrar();
-        
-        // chamar a classe Lampada com construtor Inicialização (t, p, a)
-        Lampada lampada3 = new Lampada(220, 60, true);
-        lampada3.mostrar();
+        /*
+         * // chamar a classe Lampada com construtor Inicialização (t, p)
+         * Lampada lampada2 = new Lampada(220, 60);
+         * lampada2.mostrar();
+         * 
+         * // chamar a classe Lampada com construtor Inicialização (t, p, a)
+         * Lampada lampada3 = new Lampada(220, 60, true);
+         * lampada3.mostrar();
+         */
+
+        // chamar a classe Lampada com o construtor Inicialização (tensao, potencia,
+        // ligada)
+        Lampada lampada4 = new Lampada(0, 0, false);
+        lampada4.mostrar();
+
+        // chamar a classe Lampada com o construtor v4
+        Lampada lampada5 = new Lampada(220, 60, true, false, true);
+        lampada5.mostrar();
 
     }
 
--- ProjetoLampada/src/Lampada.java
+++ ProjetoLampada/src/Lampada.java
@@ -1,3 +1,5 @@
+import java.security.InvalidParameterException;
+
 public class Lampada {
     // Atributos
     private int tensao; // 110 220
@@ -17,35 +19,174 @@ public class Lampada {
     }
 
     // construtor Inicialização (t, p)
-    public Lampada(int t, int p) {
+    public Lampada(int tensao, int potencia) {
         System.out.println("Construtor Inicialização (t, p)");
-        this.tensao = t;
-        this.potencia = p;
+        this.tensao = tensao;
+        this.potencia = potencia;
         this.ligada = false;
         this.quebrada = false;
         this.queimada = false;
     }
 
     // construtor Inicialização (t, p, a)
-    public Lampada(int t, int p, boolean a) {
-        System.out.println("Construtor Inicialização (t, p, a)");
-        this.tensao = t;
-        this.potencia = p;
-        this.ligada = a;
+    public Lampada(int tensao, int potencia, boolean ligada) {
+        System.out.println("Construtor Inicialização (tensao, potencia, ligada)");
+        this.tensao = tensao;
+        this.potencia = potencia;
+        this.ligada = ligada;
         this.quebrada = false;
         this.queimada = false;
     }
 
+    // construtor v4
+    // public Lampada(int tensao, int potencia, boolean ligada, boolean quebrada,
+    // boolean queimada) {
+    // System.out.println("Construtor v4");
+
+    // // usando os setters
+    // setTensao(tensao);
+    // setPotencia(potencia);
+    // setLigada(ligada);
+    // setQuebrada(quebrada);
+    // setQueimada(queimada);
+
+    // }
+
+    // construtor v5 usando this
+    public Lampada(int tensao, int potencia, boolean ligada, boolean quebrada, boolean queimada) {
+        this(tensao, potencia, ligada);
+        this.quebrada = quebrada;
+        this.queimada = queimada;
+        
+
+    }
+    // setter & getter
+
+    // tensao
+    public void setTensao(int tensao) {
+        if ((tensao == 110) || (tensao == 220)) {
+            this.tensao = tensao;
+        } else {
+            // System.out.println("Tensão inválida");
+            tensao = 220;
+            throw new InvalidParameterException("Tensão inválida");
+        }
+    }
+
+    public int getTensao() {
+        return this.tensao;
+    }
+
+    // potencia
+    public void setPotencia(int potencia) {
+        if (potencia == 40 || potencia == 60 || potencia == 100 || potencia == 200) {
+            this.potencia = potencia;
+        } else {
+            // System.out.println("Potência inválida");
+            potencia = 40;
+            throw new InvalidParameterException("Potência inválida");
+        }
+    }
+
+    public int getPotencia() {
+        return this.tensao;
+    }
+
+    // ligada
+    public void setLigada(boolean ligada) {
+        if (!queimada && !quebrada) {
+            this.ligada = ligada;
+        }
+
+        else {
+            if (quebrada && ligada) {
+                System.out.println("Não é possível ligar uma lâmpada quebrada");
+                queimada = true;
+            }
+            ligada = true;
+        }
+    }
+
+    public boolean isLigada() {
+        return ligada;
+    }
+
+    // quebrada
+    public void setQuebrada(boolean quebrada) {
+        if (quebrada && ligada) {
+            queimada = true;
+            ligada = false;
+            System.out.println("Não é possível quebrar uma lâmpada ligada");
+        }
+        this.quebrada = quebrada;
+    }
+
+    public boolean isQuebrada() {
+        return this.quebrada;
+    }
+
+    // queimada
+    public void setQueimada(boolean queimada) {
+        if (queimada && ligada) {
+            queimada = true;
+            ligada = false;
+            System.out.println("Não é possível queimar uma lâmpada ligada");
+        }
+        this.queimada = queimada;
+
+    }
+
+    public boolean isQueimada() {
+        return queimada;
+    }
+
+    // metodos
+
+    // ligar
+    public void ligar() {
+        setLigada(true);
+    }
+
+    // apagar
+    public void apagar() {
+        setLigada(false);
+    }
+
+    public void quebrar() {
+        setQuebrada(true);
+    }
+
+    public void queimar() {
+        setQueimada(true);
+    }
+
     public void mostrar() {
-        String texto = "Tensão: " + tensao + "V\n" + "Potencia: " + potencia + "W\n" + "Esta ligada: "
+        String texto = "Tensão: " + tensao + "V\n" + "Potencia: " + potencia + "W\n"
+                + "Esta ligada: "
                 + ((ligada) ? "Ligada\n" : "Apagada\n")
                 + "A lampada esta: " + ((quebrada) ? "Quebrada\n" : "Inteira\n")
-                + "A lampada esta: " + ((queimada) ? "Queimada\n" : "Inteira\n");
+                + "A lampada esta: " + ((queimada) ? "Queimada\n" : "Ligada\n");
 
         System.out.println(texto);
     }
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
 }
+
+/*
+ * if (quebrada) {
+ * texto += "Esta quebrada: " + ((quebrada) ? "Quebrada\n" : "Inteira\n");
+ * }
+ */
+
+/*
+ * public void mostrar() {
+ * String texto = "Tensão: " + tensao + "V\n" + "Potencia: " + potencia + "W\n"
+ * + "Esta ligada: "
+ * + ((ligada) ? "Ligada\n" : "Apagada\n")
+ * 
+ * + "A lampada esta: " + ((quebrada) ? "Quebrada\n" : "Inteira\n")
+ * + "A lampada esta: " + ((queimada) ? "Queimada\n" : "Ligada\n");
+ * 
+ * System.out.println(texto);
+ * }
+ */
