3d4bb59d4277379836bba30a60ed5e6903b89305
Carlos <101848698+Kiwiabacaxi@users.noreply.github.com>
2023-10-17 09:35:28 -0300

    feat: add new projects

---
+++ DB/pratica2/src/poov/models/Vacina.java
@@ -0,0 +1,102 @@
+package poov.models;
+
+public class Vacina {
+    private Long codigo;
+    private String nome;
+    private String descricao;
+
+    // Crie três construtores, um padrão, um de inicialização que só não recebe o
+    // código e um de inicialização que recebe tudo
+    public Vacina() {
+    }
+
+    public Vacina(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Vacina(Long codigo, String nome, String descricao) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vacina other = (Vacina) obj;
+        if (codigo == null) {
+            if (other.codigo != null)
+                return false;
+        } else if (!codigo.equals(other.codigo))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Vacina [codigo=");
+        builder.append(codigo);
+        builder.append(", nome=");
+        builder.append(nome);
+        builder.append(", descricao=");
+        builder.append(descricao);
+        builder.append("]");
+        return builder.toString();
+    }
+
+
+
+    
+
+}
